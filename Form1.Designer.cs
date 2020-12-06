26cbb76d4a2b7c2d5c47b833cf9a5a7bdf205285
Max Power <palladidrago@gmail.com>
2020-12-06 20:14:59 +0200

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace tictacthacher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label02 = new System.Windows.Forms.Label();
+            this.label01 = new System.Windows.Forms.Label();
+            this.label00 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label02
+            // 
+            this.label02.AutoSize = true;
+            this.label02.Font = new System.Drawing.Font("Impact", 110.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label02.Location = new System.Drawing.Point(523, 56);
+            this.label02.MaximumSize = new System.Drawing.Size(160, 160);
+            this.label02.MinimumSize = new System.Drawing.Size(160, 160);
+            this.label02.Name = "label02";
+            this.label02.Size = new System.Drawing.Size(160, 160);
+            this.label02.TabIndex = 10;
+            this.label02.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label02.Click += new System.EventHandler(this.Label_Click);
+            // 
+            // label01
+            // 
+            this.label01.AutoSize = true;
+            this.label01.Font = new System.Drawing.Font("Impact", 110.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label01.Location = new System.Drawing.Point(300, 56);
+            this.label01.MaximumSize = new System.Drawing.Size(160, 160);
+            this.label01.MinimumSize = new System.Drawing.Size(160, 160);
+            this.label01.Name = "label01";
+            this.label01.Size = new System.Drawing.Size(160, 160);
+            this.label01.TabIndex = 10;
+            this.label01.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label01.Click += new System.EventHandler(this.Label_Click);
+            // 
+            // label00
+            // 
+            this.label00.AutoSize = true;
+            this.label00.Font = new System.Drawing.Font("Impact", 110.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label00.Location = new System.Drawing.Point(64, 56);
+            this.label00.MaximumSize = new System.Drawing.Size(160, 160);
+            this.label00.MinimumSize = new System.Drawing.Size(160, 160);
+            this.label00.Name = "label00";
+            this.label00.Size = new System.Drawing.Size(160, 160);
+            this.label00.TabIndex = 10;
+            this.label00.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label00.Click += new System.EventHandler(this.Label_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Impact", 110.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(523, 285);
+            this.label11.MaximumSize = new System.Drawing.Size(160, 160);
+            this.label11.MinimumSize = new System.Drawing.Size(160, 160);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(160, 160);
+            this.label11.TabIndex = 10;
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label11.Click += new System.EventHandler(this.Label_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Impact", 110.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(300, 285);
+            this.label12.MaximumSize = new System.Drawing.Size(160, 160);
+            this.label12.MinimumSize = new System.Drawing.Size(160, 138);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(160, 160);
+            this.label12.TabIndex = 10;
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label12.Click += new System.EventHandler(this.Label_Click);
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Font = new System.Drawing.Font("Impact", 110.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label22.Location = new System.Drawing.Point(523, 527);
+            this.label22.MaximumSize = new System.Drawing.Size(160, 160);
+            this.label22.MinimumSize = new System.Drawing.Size(160, 160);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(160, 160);
+            this.label22.TabIndex = 10;
+            this.label22.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label22.Click += new System.EventHandler(this.Label_Click);
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Font = new System.Drawing.Font("Impact", 110.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label21.Location = new System.Drawing.Point(300, 527);
+            this.label21.MaximumSize = new System.Drawing.Size(160, 160);
+            this.label21.MinimumSize = new System.Drawing.Size(160, 160);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(160, 160);
+            this.label21.TabIndex = 10;
+            this.label21.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label21.Click += new System.EventHandler(this.Label_Click);
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.BackColor = System.Drawing.Color.Transparent;
+            this.label20.Font = new System.Drawing.Font("Impact", 110.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label20.Location = new System.Drawing.Point(55, 527);
+            this.label20.MaximumSize = new System.Drawing.Size(160, 160);
+            this.label20.MinimumSize = new System.Drawing.Size(160, 160);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(160, 160);
+            this.label20.TabIndex = 10;
+            this.label20.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label20.Click += new System.EventHandler(this.Label_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Impact", 110.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(64, 285);
+            this.label10.MaximumSize = new System.Drawing.Size(160, 160);
+            this.label10.MinimumSize = new System.Drawing.Size(160, 160);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(160, 160);
+            this.label10.TabIndex = 10;
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label10.Click += new System.EventHandler(this.Label_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BackgroundImage = global::tictacthacher.Properties.Resources.hashtag;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(22, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(717, 713);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(799, 727);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label22);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label00);
+            this.Controls.Add(this.label01);
+            this.Controls.Add(this.label02);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label02;
+        private System.Windows.Forms.Label label01;
+        private System.Windows.Forms.Label label00;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tictacthacher
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        string[,] mat;
+        char m_currentletter = 'x';
+        private void Label_Click(object sender, EventArgs e)
+        {
+            Label l = sender as Label;
+            mat = FormToMatrix();
+            if (m_currentletter == 'x')
+            {
+                l.Text = "x";
+                m_currentletter = 'o';
+                if (IsWinXorO(mat, "x")) MessageBox.Show("X won"); 
+            }
+            else
+            {
+                l.Text = "o";
+                m_currentletter = 'x';
+                if (IsWinXorO(mat, "o")) MessageBox.Show("O won"); 
+            }
+            l.Enabled = false;
+        }
+        public string[,] FormToMatrix()
+        {
+
+            //Returns matrix from labels.
+
+            string[,] m = new string[3, 3];
+            int row, col;
+            Label curLabel;
+
+            //Iterate through controls.
+
+            foreach (Control ctrl in this.Controls)
+                if (ctrl is Label)
+                {
+                    curLabel = ctrl as Label;
+
+                    //מציאת מספר השורה והעמודה מתוך שם הפקד
+
+                    row = int.Parse(curLabel.Name[curLabel.Name.Length - 2].ToString());
+                    col = int.Parse(curLabel.Name[curLabel.Name.Length - 1].ToString());
+                    m[row, col] = curLabel.Text;
+                }
+            return m;
+        }
+        public bool IsWinXorO(string[,] sMat, string s)
+        {
+
+            int countRow = 0, countColumn = 0;
+           
+            for (int i = 0; i < sMat.GetLength(0); i++)
+            {
+                countRow = 0;
+                countColumn = 0;
+                for (int j = 0; j < sMat.GetLength(1); j++)
+                {
+                    //Row check 
+                    if (sMat[i, j] == s) {countRow++; MessageBox.Show(s+" put in row"); }
+                    if (sMat[j, i] == s) {countColumn++; MessageBox.Show(s+" put in column");}
+                    if (countRow == 3 && countColumn==3 ) return true;
+                }
+            }
+            //Diagonal check
+            if (sMat[0, 0] == s && sMat[1, 1] == s && sMat[2, 2] == s
+                || sMat[0, 2] == s && sMat[1, 1] == s && sMat[2, 0] == s) return true;
+            return false;
+        }
+        private void pictureBox1_Click(object sender, EventArgs e) { }
+    }
+}
